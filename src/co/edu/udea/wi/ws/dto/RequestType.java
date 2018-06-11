a27c800b2628f41973214d35b5908d4755ad8b5f
David Santacoloma <adsi1409@hotmail.com>
2018-06-11 11:43:36 -0500

    Agregar clase vacía para consumo de tipos de servicio

---
+++ src/co/edu/udea/wi/ws/dto/RequestType.java
@@ -0,0 +1,23 @@
+package co.edu.udea.wi.ws.dto;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class RequestType {
+
+	private int ID;
+	private String Name;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+}
---
+++ src/co/edu/udea/wi/ws/rest/RequestTypeService.java
@@ -0,0 +1,11 @@
+package co.edu.udea.wi.ws.rest;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+@Path("TipoSolicitud")
+@Component
+public class RequestTypeService {
+
+}
